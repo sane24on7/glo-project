253779af54ec307890e2bdb5c9d78d38b7ac54d1
Kseniya Vasileva <vasex.xena@ya.ru>
2020-06-24 13:45:43 +0500

    feat: add hamburger-menu in html and css

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,2 +1,5 @@
 // включить при необходимости
 // @@include('./webp.js') 
+@@include('./jquery-3.5.1.min.js')
+@@include('./swiper.min.js')
+@@include('./swiper-set.js')
---
+++ #src/js/swiper-set.js
@@ -0,0 +1,6 @@
+const profitsSlider = document.querySelector('.swiper-container');
+
+let mySwiper = new Swiper (profitsSlider, {
+	slidesPerView: 1,
+	loop: true,
+})
