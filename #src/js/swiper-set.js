cd0975921bc0407a5e6176799daacbf070e93710
Kseniya Vasileva <vasex.xena@ya.ru>
2020-06-28 00:27:03 +0500

    feat: slider in profit section

--- #src/js/swiper-set.js
+++ #src/js/swiper-set.js
@@ -1,6 +1,14 @@
-const profitsSlider = document.querySelector('.swiper-container');
+const profitsSlider = document.querySelector('.profits-slider');
+const lessonsSlider = document.querySelector('.lessons-slider');
 
-let mySwiper = new Swiper (profitsSlider, {
+let profitsSwiper = new Swiper (profitsSlider, {
 	slidesPerView: 1,
 	loop: true,
+	slideClass:'profits-slide',
+	wrapperClass: 'profits-wrapper',
+	navigation: {
+    nextEl: '.profits-button-next',
+    prevEl: '.profits-button-prev',
+  },
+
 })
